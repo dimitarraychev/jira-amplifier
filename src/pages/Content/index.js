c4cac99dc328c35fa0179de706efe07f28c0a8c4
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-08-12 15:07:11 +0300

    Implemented messaging system

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,85 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+let languageDetection = false;
+
+chrome.storage.local.get('ja_languageDetection', (data) => {
+  languageDetection = data.ja_languageDetection || false;
+  console.log('lang detection: ', languageDetection);
+
+  // if (languageDetection) {
+  // 	detectLanguage();
+  // 	observeNewParagraphs(); // Start observing for new paragraphs
+  // }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log('Message received:', request);
+
+  if (request.action === 'enableLanguageDetection') {
+    languageDetection = true;
+    console.log('lang detection change: ', languageDetection);
+
+    // detectLanguage();
+    // observeNewParagraphs();
+  } else if (request.action === 'disableLanguageDetection') {
+    languageDetection = false;
+    console.log('lang detection change: ', languageDetection);
+
+    // observer.disconnect(); // Stop observing the DOM
+  }
+
+  sendResponse({ success: true });
+  return true;
+});
+
+// function detectLanguage() {
+// 	if (!languageDetection) return;
+
+// 	const paragraphs = document.querySelectorAll(".issue-link");
+
+// 	paragraphs.forEach((paragraph) => {
+// 		const text = paragraph.textContent;
+// 		if (text) {
+// 			const language = detectGermanOrEnglish(text);
+
+// 			if (language) {
+// 				paragraph.style.color = `${
+// 					language === "en" ? "#34c759" : "#ff3b30"
+// 				}`;
+// 			}
+// 		}
+// 	});
+// }
+
+// const observeNewParagraphs = () => {
+// 	const observer = new MutationObserver((mutations) => {
+// 		mutations.forEach((mutation) => {
+// 			if (mutation.addedNodes.length > 0) {
+// 				mutation.addedNodes.forEach((node) => {
+// 					if (
+// 						node.nodeType === Node.ELEMENT_NODE &&
+// 						node.matches(".issue-link")
+// 					) {
+// 						const text = node.textContent;
+// 						if (text) {
+// 							const language = detectGermanOrEnglish(text);
+
+// 							if (language) {
+// 								node.style.color = `${
+// 									language === "en" ? "#34c759" : "#ff3b30"
+// 								}`;
+// 							}
+// 						}
+// 					}
+// 				});
+// 			}
+// 		});
+// 	});
+
+// 	observer.observe(document.body, {
+// 		childList: true,
+// 		subtree: true,
+// 	});
+// };
--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import logo from '../../assets/logo.png';
+import jira from '../../assets/jira.png';
 import './Popup.css';
 import Toggle from '../../components/Toggle';
 
 const Popup = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  useEffect(() => {
+    chrome.storage.local.get('ja_languageDetection', (data) => {
+      setIsEnabled(data.ja_languageDetection || false);
+    });
+  }, []);
+
+  const toggleLanguageDetection = async () => {
+    const newState = !isEnabled;
+    setIsEnabled(newState);
+
+    chrome.storage.local.set({ ja_languageDetection: newState });
+
+    try {
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true,
+      });
+
+      if (!tab?.id) return;
+
+      const response = await chrome.tabs.sendMessage(tab.id, {
+        action: newState
+          ? 'enableLanguageDetection'
+          : 'disableLanguageDetection',
+      });
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <img src={jira} alt="logo" />
+        <img src={logo} alt="logo" />
         <h1>Jira Amplifier</h1>
       </header>
-      <Toggle label="Language Detection" />
-      <Toggle label="Language Detection" />
+      <Toggle label="Language Detection" onClick={toggleLanguageDetection} />
     </div>
   );
 };
