f61d4c4a84f8eed3df1d5746fcc000f0cd42627d
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-08-12 14:04:50 +0300

    Assets update

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// import logo from '../../assets/img/logo.svg';
-// import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 import Toggle from '../../components/Toggle';
 
@@ -10,6 +8,7 @@ const Popup = () => {
       <header className="App-header">
         <h1>Jira Amplifier</h1>
       </header>
+      <Toggle label="Language Detection" />
       <Toggle label="Language Detection" />
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -169,7 +169,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-128.png',
+          from: 'src/assets/icon-128.png',
           to: path.join(__dirname, 'build'),
           force: true,
         },
@@ -178,7 +178,16 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-34.png',
+          from: 'src/assets/icon-48.png',
+          to: path.join(__dirname, 'build'),
+          force: true,
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets/icon-16.png',
           to: path.join(__dirname, 'build'),
           force: true,
         },
