458dbeff9c417f85261cd6f0f3c218e3bf4be94b
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-08-12 16:56:34 +0300

    Added observer for new tickets

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,30 +1,30 @@
 import { franc } from 'franc-min';
 
-console.log('Content script works!');
-
 let languageDetection = false;
+let tableObserver = new MutationObserver(() => {});
 
 chrome.storage.local.get('ja_languageDetection', (data) => {
   languageDetection = data.ja_languageDetection || false;
-  console.log('lang detection: ', languageDetection);
 
   if (languageDetection) {
     detectLanguage();
-    // observeNewParagraphs();
+    observeNewParagraphs();
   }
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log('Message received:', request);
+  const actions = ['enableLanguageDetection', 'disableLanguageDetection'];
+  if (!actions.includes(request.action)) {
+    sendResponse({ success: false });
+    return true;
+  }
 
-  if (request.action === 'enableLanguageDetection') {
+  if (request.action === actions[0]) {
     languageDetection = true;
-    console.log('lang detection change: ', languageDetection);
-    // observeNewParagraphs();
-  } else if (request.action === 'disableLanguageDetection') {
+    observeNewParagraphs();
+  } else if (request.action === actions[1]) {
     languageDetection = false;
-    console.log('lang detection change: ', languageDetection);
-    // observer.disconnect(); // Stop observing the DOM
+    tableObserver.disconnect();
   }
 
   detectLanguage();
@@ -32,62 +32,52 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   return true;
 });
 
-function detectLanguage() {
+const detectLanguage = () => {
   const paragraphs = document.querySelectorAll('.issue-link');
 
   if (!languageDetection) {
-    paragraphs.forEach((paragraph) => {
-      paragraph.style.color = '#6e6e6e';
-    });
+    paragraphs.forEach((paragraph) => (paragraph.style.color = '#6e6e6e'));
     return;
   }
 
-  paragraphs.forEach((paragraph) => {
-    const text = paragraph.textContent;
-    if (text) {
-      const language = franc(text, { only: ['eng', 'deu'], minLength: 5 });
-      console.log(text, language);
+  paragraphs.forEach((paragraph) => styleParagraph(paragraph));
+};
 
-      if (language) {
-        paragraph.style.color = `${
-          language === 'eng'
-            ? '#34c759'
-            : language === 'deu'
-            ? '#ff3b30'
-            : '#6e6e6e'
-        }`;
+const styleParagraph = (paragraph) => {
+  const text = paragraph.textContent;
+  if (!text) return;
+
+  const language = franc(text, { only: ['eng', 'deu'], minLength: 5 });
+  if (!language) return;
+
+  paragraph.style.color = `${
+    language === 'eng' ? '#34c759' : language === 'deu' ? '#ff3b30' : '#6e6e6e'
+  }`;
+};
+
+const observeNewParagraphs = () => {
+  const tableContainer = document.querySelector('.queue-react-table-container');
+
+  if (!tableContainer) {
+    console.warn('Target div not found. MutationObserver not initialized.');
+    return;
+  }
+
+  const tableObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.addedNodes.length > 0) {
+        mutation.addedNodes.forEach((node) => {
+          const isValidNode =
+            node.nodeType === Node.ELEMENT_NODE && node.matches('.issue-link');
+
+          if (isValidNode) styleParagraph(node);
+        });
       }
-    }
+    });
   });
-}
 
-// const observeNewParagraphs = () => {
-// 	const observer = new MutationObserver((mutations) => {
-// 		mutations.forEach((mutation) => {
-// 			if (mutation.addedNodes.length > 0) {
-// 				mutation.addedNodes.forEach((node) => {
-// 					if (
-// 						node.nodeType === Node.ELEMENT_NODE &&
-// 						node.matches(".issue-link")
-// 					) {
-// 						const text = node.textContent;
-// 						if (text) {
-// 							const language = detectGermanOrEnglish(text);
-
-// 							if (language) {
-// 								node.style.color = `${
-// 									language === "en" ? "#34c759" : "#ff3b30"
-// 								}`;
-// 							}
-// 						}
-// 					}
-// 				});
-// 			}
-// 		});
-// 	});
-
-// 	observer.observe(document.body, {
-// 		childList: true,
-// 		subtree: true,
-// 	});
-// };
+  tableObserver.observe(tableContainer, {
+    childList: true,
+    subtree: true,
+  });
+};
