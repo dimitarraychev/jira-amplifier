11c5cfb740b27808df14871bb2dfd2fd4555345a
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-08-12 15:49:31 +0300

    Initiated basic lang checks with buttons

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -8,10 +8,10 @@ chrome.storage.local.get('ja_languageDetection', (data) => {
   languageDetection = data.ja_languageDetection || false;
   console.log('lang detection: ', languageDetection);
 
-  // if (languageDetection) {
-  // 	detectLanguage();
-  // 	observeNewParagraphs(); // Start observing for new paragraphs
-  // }
+  if (languageDetection) {
+    detectLanguage();
+    // observeNewParagraphs();
+  }
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -20,38 +20,46 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'enableLanguageDetection') {
     languageDetection = true;
     console.log('lang detection change: ', languageDetection);
-
-    // detectLanguage();
     // observeNewParagraphs();
   } else if (request.action === 'disableLanguageDetection') {
     languageDetection = false;
     console.log('lang detection change: ', languageDetection);
-
     // observer.disconnect(); // Stop observing the DOM
   }
 
+  detectLanguage();
   sendResponse({ success: true });
   return true;
 });
 
-// function detectLanguage() {
-// 	if (!languageDetection) return;
+function detectLanguage() {
+  const paragraphs = document.querySelectorAll('.issue-link');
 
-// 	const paragraphs = document.querySelectorAll(".issue-link");
+  if (!languageDetection) {
+    paragraphs.forEach((paragraph) => {
+      paragraph.style.color = '#6e6e6e';
+    });
+    return;
+  }
 
-// 	paragraphs.forEach((paragraph) => {
-// 		const text = paragraph.textContent;
-// 		if (text) {
-// 			const language = detectGermanOrEnglish(text);
+  paragraphs.forEach((paragraph) => {
+    const text = paragraph.textContent;
+    if (text) {
+      const language = franc(text, { only: ['eng', 'deu'], minLength: 5 });
+      console.log(text, language);
 
-// 			if (language) {
-// 				paragraph.style.color = `${
-// 					language === "en" ? "#34c759" : "#ff3b30"
-// 				}`;
-// 			}
-// 		}
-// 	});
-// }
+      if (language) {
+        paragraph.style.color = `${
+          language === 'eng'
+            ? '#34c759'
+            : language === 'deu'
+            ? '#ff3b30'
+            : '#6e6e6e'
+        }`;
+      }
+    }
+  });
+}
 
 // const observeNewParagraphs = () => {
 // 	const observer = new MutationObserver((mutations) => {
